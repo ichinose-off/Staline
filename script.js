fb1dfcabc484f12bc1b127daa455a7db0f13ca2f
ichinose-off <119628996+ichinose-off@users.noreply.github.com>
2022-12-02 16:39:04 +0400

    first commit

---
+++ script.js
@@ -0,0 +1,15 @@
+"use strict";
+
+window.addEventListener("load", () => {
+  document.getElementById("loader").classList.remove("active");
+  document.body.classList.remove("cutY");
+  const msg = "En vue du peu de temps disponible à la création de ce site web. celui-ci reste assez médiocre, nos excuses...";
+  alert(msg);
+})
+
+const musicPath = "music.mp3";
+
+const audio = new Audio(musicPath);
+audio.loop = true;
+audio.autoplay = true;
+audio.play();
