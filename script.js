f52632fcf50af970e9e45863336a290537a7a082
ichinose-off <119628996+ichinose-off@users.noreply.github.com>
2024-01-01 18:00:47 +0400

    Update script.js
    Adding personnal message

--- script.js
+++ script.js
@@ -5,6 +5,11 @@ window.addEventListener("load", () => {
   document.body.classList.remove("cutY");
   const msg = "En vue du peu de temps disponible à la création de ce site web. celui-ci reste assez médiocre, nos excuses...";
   alert(msg);
+
+
+  # Update January first
+  alert("Bonjour Madame ! Vous utilisez toujours notre site ! Faîtes-le nous savoir par mail : lucastecher97421fabien@gmail.com  Bonne Journée !"):
+  
 })
 
 const musicPath = "res/music.mp3";
